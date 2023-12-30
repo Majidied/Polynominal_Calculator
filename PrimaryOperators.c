690861193c6a59a84933e8dea4ef0a4e6ee964f4
mohammed majidi <majidimajidi2003@outlook.com>
2023-12-30 13:27:57 +0100

    new

--- PrimaryOperators.c
+++ PrimaryOperators.c
@@ -1,14 +1,14 @@
 #include "poly_project.h"
 
-poly_table_t *poly_table_create(unsigned long int size) {
-    poly_table_t *pt = malloc(sizeof(poly_table_t));
+poly_name_table_t *poly_name_table_create(unsigned long int size) {
+    poly_name_table_t *pt = malloc(sizeof(poly_name_table_t));
     if (pt == NULL) {
         perror("Failed to allocate memory for polynomial table");
         exit(EXIT_FAILURE);
     }
 
     pt->size = size;
-    pt->array = calloc(size, sizeof(poly_node_t *));
+    pt->array = calloc(size, sizeof(poly_name_node_t *));
     if (pt->array == NULL) {
         perror("Failed to allocate memory for polynomial table array");
         exit(EXIT_FAILURE);
@@ -17,40 +17,55 @@ poly_table_t *poly_table_create(unsigned long int size) {
     return pt;
 }
 
-unsigned long int poly_hash(int degree) {
-    // Simple hash function using the degree as the key
+unsigned long int poly_name_hash(const char *name) {
+    // Simple hash function using the name as the key
     // You might need a more sophisticated hash function depending on your requirements
-    return (unsigned long int)degree;
+    unsigned long int hash = 5381;
+    int c;
+
+    while ((c = *name++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash;
 }
 
-unsigned long int poly_key_index(int degree, unsigned long int size) {
-    return poly_hash(degree) % size;
+unsigned long int poly_name_key_index(const char *name, unsigned long int size) {
+    return poly_name_hash(name) % size;
 }
 
-int poly_table_set(poly_table_t *pt, int degree, double coefficient) {
-    unsigned long int index = poly_key_index(degree, pt->size);
+int poly_name_table_set(poly_name_table_t *pt, const char *name, int degree, double coefficient) {
+    unsigned long int index = poly_name_key_index(name, pt->size);
 
-    poly_node_t *new_node = malloc(sizeof(poly_node_t));
+    poly_name_node_t *new_node = malloc(sizeof(poly_name_node_t));
     if (new_node == NULL) {
         perror("Failed to allocate memory for polynomial node");
         exit(EXIT_FAILURE);
     }
 
-    new_node->degree = degree;
-    new_node->coefficient = coefficient;
-    new_node->next = pt->array[index];
+    strncpy(new_node->name, name, sizeof(new_node->name) - 1);
+    new_node->name[sizeof(new_node->name) - 1] = '\0';
+    new_node->poly = malloc(sizeof(poly_node_t));
+    if (new_node->poly == NULL) {
+        perror("Failed to allocate memory for polynomial node");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->poly->degree = degree;
+    new_node->poly->coefficient = coefficient;
+    new_node->poly->next = pt->array[index];
     pt->array[index] = new_node;
 
     return 0;  // You might want to handle errors differently
 }
 
-double poly_table_get(const poly_table_t *pt, int degree) {
-    unsigned long int index = poly_key_index(degree, pt->size);
+double poly_name_table_get(const poly_name_table_t *pt, const char *name, int degree) {
+    unsigned long int index = poly_name_key_index(name, pt->size);
 
-    poly_node_t *current = pt->array[index];
+    poly_name_node_t *current = pt->array[index];
     while (current != NULL) {
-        if (current->degree == degree) {
-            return current->coefficient;
+        if (strcmp(current->name, name) == 0 && current->poly->degree == degree) {
+            return current->poly->coefficient;
         }
         current = current->next;
     }
@@ -58,23 +73,24 @@ double poly_table_get(const poly_table_t *pt, int degree) {
     return 0.0;  // Default coefficient if not found
 }
 
-void poly_table_print(const poly_table_t *pt) {
+void poly_name_table_print(const poly_name_table_t *pt) {
     for (unsigned long int i = 0; i < pt->size; ++i) {
-        poly_node_t *current = pt->array[i];
+        poly_name_node_t *current = pt->array[i];
         while (current != NULL) {
-            printf("(%dx^%d) ", (int)current->coefficient, current->degree);
+            printf("%s: (%dx^%d) ", current->name, (int)current->poly->coefficient, current->poly->degree);
             current = current->next;
         }
     }
     printf("\n");
 }
 
-void poly_table_delete(poly_table_t *pt) {
+void poly_name_table_delete(poly_name_table_t *pt) {
     for (unsigned long int i = 0; i < pt->size; ++i) {
-        poly_node_t *current = pt->array[i];
+        poly_name_node_t *current = pt->array[i];
         while (current != NULL) {
-            poly_node_t *temp = current;
+            poly_name_node_t *temp = current;
             current = current->next;
+            free(temp->poly);
             free(temp);
         }
     }
