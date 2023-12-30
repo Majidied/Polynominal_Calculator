20e2ac4d0e8e2232bbc2ef3578b2197b3838a4fe
mohammed majidi <majidimajidi2003@outlook.com>
2023-12-30 13:23:52 +0100

    new

--- Poly_project.h
+++ Poly_project.h
@@ -33,12 +33,40 @@ typedef struct poly_table_s
     poly_node_t **array;
 } poly_table_t;
 
-poly_table_t *poly_table_create(unsigned long int size);
-unsigned long int poly_hash(int degree);
-unsigned long int poly_key_index(int degree, unsigned long int size);
-int poly_table_set(poly_table_t *pt, int degree, double coefficient);
-double poly_table_get(const poly_table_t *pt, int degree);
-void poly_table_print(const poly_table_t *pt);
-void poly_table_delete(poly_table_t *pt);
+/**
+ * struct poly_name_node_s - Node of a polynomial with a name as a key
+ *
+ * @name: The name of the polynomial
+ * @poly: A pointer to the polynomial represented by a linked list of terms
+ * @next: A pointer to the next node of the PolynomialTable
+ */
+typedef struct poly_name_node_s
+{
+    char name[20];  // Assuming a maximum name length of 19 characters
+    poly_node_t *poly;
+    struct poly_name_node_s *next;
+} poly_name_node_t;
+
+/**
+ * struct poly_name_table_s - Polynomial table with names as keys
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our PolynomialTable to use a Chaining collision handling
+ */
+typedef struct poly_name_table_s
+{
+    unsigned long int size;
+    poly_name_node_t **array;
+} poly_name_table_t;
+
+poly_name_table_t *poly_name_table_create(unsigned long int size);
+unsigned long int poly_name_hash(const char *name);
+unsigned long int poly_name_key_index(const char *name, unsigned long int size);
+int poly_name_table_set(poly_name_table_t *pt, const char *name, int degree, double coefficient);
+double poly_name_table_get(const poly_name_table_t *pt, const char *name, int degree);
+void poly_name_table_print(const poly_name_table_t *pt);
+void poly_name_table_delete(poly_name_table_t *pt);
 
 #endif
--- PrimaryOperators.c
+++ PrimaryOperators.c
@@ -1,212 +1,83 @@
-#include "Poly_project.h"
+#include "poly_project.h"
 
-/**
- * hash_table_create - creates a hash table.
- * @size: size of the array.
- * Return: pointer to the newly craeted hash table.
- */
+poly_table_t *poly_table_create(unsigned long int size) {
+    poly_table_t *pt = malloc(sizeof(poly_table_t));
+    if (pt == NULL) {
+        perror("Failed to allocate memory for polynomial table");
+        exit(EXIT_FAILURE);
+    }
 
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *hash_table;
-	unsigned long int i;
+    pt->size = size;
+    pt->array = calloc(size, sizeof(poly_node_t *));
+    if (pt->array == NULL) {
+        perror("Failed to allocate memory for polynomial table array");
+        exit(EXIT_FAILURE);
+    }
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
-		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_table->array == NULL)
-	{
-		free(hash_table);
-		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	return (hash_table);
+    return pt;
 }
 
-/**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
- */
-unsigned long int hash_djb2(const unsigned char *str)
-{
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash);
+unsigned long int poly_hash(string name) {
+    unsigned long int hash_value = 5381;
+    return (unsigned long int)name;
 }
 
-/**
- * key_index - calculate the index of the key.
- * @key: the key.
- * @size: the size of the array of the hash table.
- * Return: the index at which the key/value pair.
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-	return (hash_djb2(key) % size);
+unsigned long int poly_key_index(int degree, unsigned long int size) {
+    return poly_hash(degree) % size;
 }
 
+int poly_table_set(poly_table_t *pt, int degree, double coefficient) {
+    unsigned long int index = poly_key_index(degree, pt->size);
 
-/**
- * make_hash_node - creates a new hash node.
- * @key: key for the node.
- * @value: for the node.
- * Return: the new node, or NULL on failure.
- */
-hash_node_t *make_hash_node(const char *key, const char *value)
-{
-	hash_node_t *node;
+    poly_node_t *new_node = malloc(sizeof(poly_node_t));
+    if (new_node == NULL) {
+        perror("Failed to allocate memory for polynomial node");
+        exit(EXIT_FAILURE);
+    }
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
-		return (NULL);
-	node->key = strdup(key);
-	if (node->key == NULL)
-	{
-		free(node);
-		return (NULL);
-	}
-	node->value = strdup(value);
-	if (node->value == NULL)
-	{
-		free(node->key);
-		free(node);
-		return (NULL);
-	}
-	node->next = NULL;
-	return (node);
+    new_node->degree = degree;
+    new_node->coefficient = coefficient;
+    new_node->next = pt->array[index];
+    pt->array[index] = new_node;
+
+    return 0;  // You might want to handle errors differently
 }
 
-/**
- * hash_table_set - sets a key to a value in the hash table.
- * @ht: hash table to add element to.
- * @key: key for the data.
- * @value: data ton store.
- * Return: 1 if seccessful, o otherwise.
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *hash_node, *tmp;
-	char *new_value;
+double poly_table_get(const poly_table_t *pt, int degree) {
+    unsigned long int index = poly_key_index(degree, pt->size);
 
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-		key == NULL || strlen(key) == 0 || value == NULL)
-		return (0);
-	index = key_index((const unsigned char *) key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
-		}
-		tmp = tmp->next;
-	}
-	hash_node = make_hash_node(key, value);
-	if (hash_node == NULL)
-		return (0);
-	hash_node->next = ht->array[index];
-	ht->array[index] = hash_node;
-	return (1);
+    poly_node_t *current = pt->array[index];
+    while (current != NULL) {
+        if (current->degree == degree) {
+            return current->coefficient;
+        }
+        current = current->next;
+    }
+
+    return 0.0;  // Default coefficient if not found
 }
 
-
-/**
- * hash_table_get - retrieves a value associated with a key.
- * @ht: table to retrieve value from.
- * @key: key to find value.
- * Return: value associated with key, or NULL if key cannot be found.
- */
-char *hash_table_get(const hash_table_t *ht, const char *key)
-{
-	unsigned long int index;
-	hash_node_t *tmp;
-
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-			key == NULL || strlen(key) == 0)
-		return (NULL);
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-			return (tmp->value);
-		tmp = tmp->next;
-	}
-	return (NULL);
+void poly_table_print(const poly_table_t *pt) {
+    for (unsigned long int i = 0; i < pt->size; ++i) {
+        poly_node_t *current = pt->array[i];
+        while (current != NULL) {
+            printf("(%dx^%d) ", (int)current->coefficient, current->degree);
+            current = current->next;
+        }
+    }
+    printf("\n");
 }
 
-/**
- * hash_table_print - prints a hash table.
- * @ht: hash table to print.
- * Return: void.
- */
+void poly_table_delete(poly_table_t *pt) {
+    for (unsigned long int i = 0; i < pt->size; ++i) {
+        poly_node_t *current = pt->array[i];
+        while (current != NULL) {
+            poly_node_t *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
 
-void hash_table_print(const hash_table_t *ht)
-{
-	unsigned long int i;
-	hash_node_t *tmp;
-	char flag = 0;
-
-	if (ht == NULL || ht->array == NULL)
-		return;
-	printf("{");
-	for (i = 0; i < ht->size; i++)
-	{
-		tmp = ht->array[i];
-		while (tmp != NULL)
-		{
-			if (flag == 1)
-				printf(", ");
-			printf("'%s': '%s'", tmp->key, tmp->value);
-			flag = 1;
-			tmp = tmp->next;
-		}
-	}
-	printf("}\n");
-}
-
-/**
- * hash_table_delete - delete a hash table created.
- * @ht: hash table to delete.
- * Return: Nothing.
- */
-void hash_table_delete(hash_table_t *ht)
-{
-	unsigned long int i;
-	hash_node_t *next;
-
-	if (ht == NULL || ht->array == NULL || ht->size == 0)
-		return;
-	for (i = 0; i < ht->size; i++)
-	{
-		while (ht->array[i] != NULL)
-		{
-			next = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = next;
-		}
-	}
-	free(ht->array);
-	ht->array = NULL;
-	ht->size = 0;
-	free(ht);
+    free(pt->array);
+    free(pt);
 }
