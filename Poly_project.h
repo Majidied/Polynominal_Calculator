9b0d2daf91c3c03026f16e5acebf5c56dacefa9a
mohammed majidi <majidimajidi2003@outlook.com>
2024-01-13 17:36:01 +0100

    update not fixed

--- Poly_project.h
+++ Poly_project.h
@@ -1,8 +1,8 @@
 #ifndef POLY_PROJECT_H
 #define POLY_PROJECT_H
 
-#include "libc.h"
 #include <stdlib.h>
+#include <io.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -37,8 +37,9 @@ poly_node_t *create_polynome(char *name);
 void poly_set(hash_table_t *pt, char *key, char *new_name);
 size_t poly_hash(char *name);
 size_t poly_key_index(char *name, size_t size);
-void poly_table_set(poly_name_table_t *pt, char *name);
-void poly_table_print(hash_table_t *pt);
+void poly_table_set(hash_table_t *pt, char *name);
+void poly_table_print(hash_table_t *pt, char *key);
 void poly_table_delete(hash_table_t *pt);
+int findlen(int num);
 
 #endif
--- PrimaryOperators.c
+++ PrimaryOperators.c
@@ -1,4 +1,4 @@
-#include "poly_project.h"
+#include "Poly_project.h"
 
 hash_table_t *poly_table_create(size_t size)
 {
@@ -19,7 +19,7 @@ hash_table_t *poly_table_create(size_t size)
     return pt;
 }
 
-size_t poly_hash(const char *name) {
+size_t poly_hash(char *name) {
     int c;
     size_t hash = 5381;
 
@@ -31,7 +31,7 @@ size_t poly_hash(const char *name) {
     return hash;
 }
 
-size_t poly_key_index(const char *name, size_t size)
+size_t poly_key_index(char *name, size_t size)
 {
     return poly_hash(name) % size;
 }
@@ -41,15 +41,22 @@ poly_node_t *create_polynome(char *name)
     poly_node_t *monome, *head;
     int found_x;
 
+    if (name == NULL || *name == '\0') {
+        fprintf(stderr, "Error: Name is NULL\n");
+        return NULL;
+    }
     monome = malloc(sizeof(poly_node_t));
     if (!monome)
     {
         perror("Failed to allocate memory for polynomial node");
         exit(EXIT_FAILURE);
     }
+    monome->next = NULL;
+    monome->coff.down = 1;
+    monome->coff.up = 0;
     head = monome;
     found_x = 0;
-    while (*name)
+    while (*name != '\0')
     {
         if (*name <= '9' && *name >= '0' || *name == '-' || *name == '+')
         {
@@ -58,7 +65,7 @@ poly_node_t *create_polynome(char *name)
                 if (*(name - 1) == '^')
                 {
                     monome->pow = atoi(name);
-                    name += strlen(itoa(monome->pow));
+                    name += findlen(monome->pow);
                 }
                 else
                     monome->pow = 1;
@@ -70,22 +77,26 @@ poly_node_t *create_polynome(char *name)
                 }
                 found_x = 0;
                 monome = monome->next;
+                monome->next = NULL;
+                monome->coff.down = 1;
+                monome->coff.up = 0;
+                name++;
             }
             else
             {
                 monome->coff.up = atoi(name);
                 if (*name == '+')
                     name++;
-                name += strlen(itoa(monome->coff.up));
+                name += findlen(monome->coff.up);
                 if (*name == '/')
                 {
                     name++;
                     monome->coff.down = atoi(name);
-                    name += strlen(itoa(monome->coff.down));
+                    name += findlen(monome->coff.down);
                 }
                 else
                     monome->coff.down = 1;
-                if (*name != 'x' || *name != 'X')
+                if (*name != 'x' && *name != 'X')
                     monome->pow = 0;
             }
         }
@@ -94,13 +105,15 @@ poly_node_t *create_polynome(char *name)
             if (!monome->coff.up)
                 monome->coff.up = 1;
             found_x = 1;
+            name++;
         }
         else
             name++;
     }
+    return (head);
 }
 
-void    poly_table_set(hash_table_t *pt, const char *name)
+void poly_table_set(hash_table_t *pt, char *name)
 {
     poly_node_t *polynome;
     hash_node_t *new_node;
@@ -128,11 +141,11 @@ void    poly_table_set(hash_table_t *pt, const char *name)
         perror("Failed to allocate memory for polynomial node");
         exit(EXIT_FAILURE);
     }
-    new_node->poly = polynome;
+    new_node->poly = &polynome;
     pt->array[index] = new_node;
 }
 
-void poly_table_print(const hash_table_t *pt, char *key)
+void poly_table_print(hash_table_t *pt, char *key)
 {
     size_t i = poly_key_index(key, pt->size);
     poly_node_t **tmp = pt->array[i]->poly;
@@ -160,12 +173,14 @@ void poly_set(hash_table_t *pt, char *key, char *new_name)
 {
     size_t i = poly_key_index(key, pt->size);
     poly_node_t **tmp = pt->array[i]->poly;
-    poly_node_t **p_tmp = pt->array[i]->poly;
-    poly_node_t *new = *tmp;
+    poly_node_t *new;
 
-    new = create_polynome(new_name);
-    p_tmp = &new;
-    free(tmp);
+    *(pt->array[i]->poly) = create_polynome(new_name);
+    while (*tmp){
+        new = (*tmp)->next;
+        free(*tmp);
+        *tmp = new;
+    }
 }
 
 void poly_table_delete(hash_table_t *pt)
