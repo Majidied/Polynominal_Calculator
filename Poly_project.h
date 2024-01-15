f000793e65879b94d2b4d36b1a55937cf59a7ca9
mohammed majidi <majidimajidi2003@outlook.com>
2024-01-15 12:32:14 +0100

    update fix LET

--- Poly_project.h
+++ Poly_project.h
@@ -1,29 +1,28 @@
 #ifndef POLY_PROJECT_H
 #define POLY_PROJECT_H
 
-#include <stdlib.h>
-#include <io.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct quot_s
 {
-	int up;
-	int down;
- } quot;
+    int up;
+    int down;
+} quot;
 
 typedef struct poly_node_s
 {
-	int pow;
-	quot coff;
-	struct poly_node_s *next;
+    int pow;
+    quot coff;
+    struct poly_node_s *next;
 } poly_node_t;
-
 
 typedef struct hash_node_s
 {
-	char *key;
-	poly_node_t **poly;
+    char *key;
+    poly_node_t *poly;
 } hash_node_t;
 
 typedef struct hash_table_s
@@ -31,13 +30,12 @@ typedef struct hash_table_s
     unsigned long int size;
     hash_node_t **array;
 } hash_table_t;
-
 hash_table_t *poly_table_create(size_t size);
 poly_node_t *create_polynome(char *name);
 void poly_set(hash_table_t *pt, char *key, char *new_name);
 size_t poly_hash(char *name);
 size_t poly_key_index(char *name, size_t size);
-void poly_table_set(hash_table_t *pt, char *name);
+void poly_table_set(hash_table_t *pt, char *id, char *name);
 void poly_table_print(hash_table_t *pt, char *key);
 void poly_table_delete(hash_table_t *pt);
 int findlen(int num);
--- PrimaryOperators.c
+++ PrimaryOperators.c
@@ -3,7 +3,8 @@
 hash_table_t *poly_table_create(size_t size)
 {
     hash_table_t *pt = malloc(sizeof(hash_table_t));
-    if (pt == NULL) {
+    if (pt == NULL)
+    {
         perror("Failed to allocate memory for polynomial table");
         exit(EXIT_FAILURE);
     }
@@ -11,7 +12,8 @@ hash_table_t *poly_table_create(size_t size)
     pt->size = size;
     pt->array = calloc(size, sizeof(hash_node_t *));
 
-    if (pt->array == NULL) {
+    if (pt->array == NULL)
+    {
         perror("Failed to allocate memory for polynomial table array");
         exit(EXIT_FAILURE);
     }
@@ -19,12 +21,13 @@ hash_table_t *poly_table_create(size_t size)
     return pt;
 }
 
-size_t poly_hash(char *name) {
+size_t poly_hash(char *name)
+{
     int c;
     size_t hash = 5381;
 
-
-    while ((c = *name++)) {
+    while ((c = *name++))
+    {
         hash = ((hash << 5) + hash) + c;
     }
 
@@ -36,151 +39,252 @@ size_t poly_key_index(char *name, size_t size)
     return poly_hash(name) % size;
 }
 
-poly_node_t *create_polynome(char *name)
+poly_node_t *addTerm(poly_node_t *poly, poly_node_t *term)
 {
-    poly_node_t *monome, *head;
-    int found_x;
+    poly_node_t *current = poly;
+    poly_node_t *prev = NULL;
 
-    if (name == NULL || *name == '\0') {
-        fprintf(stderr, "Error: Name is NULL\n");
-        return NULL;
+    // Traverse the list to find the correct position
+    while (current != NULL && current->pow > term->pow) {
+        prev = current;
+        current = current->next;
     }
-    monome = malloc(sizeof(poly_node_t));
-    if (!monome)
-    {
-        perror("Failed to allocate memory for polynomial node");
-        exit(EXIT_FAILURE);
+
+    // If a term with the same power already exists, combine coefficients
+    if (current != NULL && current->pow == term->pow) {
+        current->coff.up += term->coff.up;
+        // Adjust any necessary cleanup for the new term (term is not used anymore)
+        free(term);
+        return poly;
     }
-    monome->next = NULL;
-    monome->coff.down = 1;
-    monome->coff.up = 0;
-    head = monome;
-    found_x = 0;
-    while (*name != '\0')
-    {
-        if (*name <= '9' && *name >= '0' || *name == '-' || *name == '+')
-        {
-            if (found_x)
-            {
-                if (*(name - 1) == '^')
-                {
-                    monome->pow = atoi(name);
-                    name += findlen(monome->pow);
-                }
-                else
-                    monome->pow = 1;
-                monome->next = malloc(sizeof(poly_node_t));
-                if (!monome->next)
-                {
-                    perror("Failed to allocate memory for polynomial node");
-                    exit(EXIT_FAILURE);
-                }
-                found_x = 0;
-                monome = monome->next;
-                monome->next = NULL;
-                monome->coff.down = 1;
-                monome->coff.up = 0;
-                name++;
-            }
-            else
-            {
-                monome->coff.up = atoi(name);
-                if (*name == '+')
-                    name++;
-                name += findlen(monome->coff.up);
-                if (*name == '/')
-                {
-                    name++;
-                    monome->coff.down = atoi(name);
-                    name += findlen(monome->coff.down);
-                }
-                else
-                    monome->coff.down = 1;
-                if (*name != 'x' && *name != 'X')
-                    monome->pow = 0;
-            }
-        }
-        else if (*name == 'x' || *name == 'X')
-        {
-            if (!monome->coff.up)
-                monome->coff.up = 1;
-            found_x = 1;
-            name++;
-        }
-        else
-            name++;
+
+    // If prev is NULL, the new term has the highest power and should be the new head
+    if (prev == NULL) {
+        term->next = poly;
+        return term;
     }
-    return (head);
+
+    // Insert the new term at the correct position
+    prev->next = term;
+    term->next = current;
+
+    return poly;
 }
 
-void poly_table_set(hash_table_t *pt, char *name)
+
+void parseNumber(const char *str, int *numerator, int *denominator)
 {
-    poly_node_t *polynome;
-    hash_node_t *new_node;
-    size_t      index;
-    char        id[20];
-    int         i = 0;
-    while (*name != '=')
+    char temp[100];
+    int i = 0;
+
+    while (*str != '+' && *str != '-' && *str != '\0')
     {
-        id[i++] = name[0];
-        name++;
+        temp[i] = *str;
+        str++;
+        i++;
     }
-    polynome = create_polynome(name);
-    index = poly_key_index(id, pt->size);
-    new_node = malloc(sizeof(hash_node_t));
+
+    temp[i] = '\0';
+
+    if (strchr(temp, '/'))
+    {
+        sscanf(temp, "%d/%d", numerator, denominator);
+    }
+    else if (sscanf(temp, "%d", numerator) == 1)
+    {
+        *denominator = 1;
+    }
+    else
+    {
+        *numerator = 1;
+        *denominator = 1;
+    }
+}
+
+poly_node_t *create_polynome(char *input)
+{
+    poly_node_t *poly = NULL;
+
+    while (*input == ' ')
+    {
+        input++;
+    }
+
+    while (*input != '\0')
+    {
+        int isNegative = 0;
+
+        if (*input == '-')
+        {
+            isNegative = 1;
+            input++;
+        }
+        else if (*input == '+')
+        {
+            input++;
+        }
+
+        poly_node_t *term = (poly_node_t *)malloc(sizeof(poly_node_t));
+        term->pow = 0;
+        term->next = NULL;
+
+        char temp[100];
+        int i = 0;
+
+        while (*input != '+' && *input != '-' && *input != '\0')
+        {
+            temp[i] = *input;
+            input++;
+            i++;
+        }
+
+        temp[i] = '\0';
+
+        char *varPart = strchr(temp, 'X');
+
+        if (varPart != NULL)
+        {
+            if (sscanf(varPart, "X^%d", &term->pow) != 1)
+            {
+                term->pow = 1;
+            }
+        }
+
+        int coefNumerator, coefDenominator;
+        parseNumber(temp, &coefNumerator, &coefDenominator);
+
+        term->coff.up = isNegative ? -coefNumerator : coefNumerator;
+        term->coff.down = coefDenominator;
+
+        poly = addTerm(poly, term);
+
+        size_t length = strcspn(input, "+-");
+        input += length;
+    }
+    return poly;
+}
+
+void poly_table_set(hash_table_t *pt, char *id, char *name)
+{
+    // Create a new polynomial node and copy the polynomial data
+    poly_node_t *polynome = create_polynome(name);
+
+    // Find the index in the hash table array
+    size_t index = poly_key_index(id, pt->size);
+
+    // Create a new hash node and allocate memory for the key
+    hash_node_t *new_node = malloc(sizeof(hash_node_t));
     if (new_node == NULL)
     {
         perror("Failed to allocate memory for polynomial node");
         exit(EXIT_FAILURE);
     }
-    strncpy(new_node->key, id, strlen(id) - 1);
-    new_node->key[strlen(new_node->key) - 1] = '\0';
-    new_node->poly = malloc(sizeof(poly_node_t *));
-    if (new_node->poly == NULL)
+
+    // Allocate memory for the key and copy it
+    new_node->key = malloc(strlen(id) + 1);
+    if (new_node->key == NULL)
     {
-        perror("Failed to allocate memory for polynomial node");
+        perror("Failed to allocate memory for key");
         exit(EXIT_FAILURE);
     }
-    new_node->poly = &polynome;
+    strcpy(new_node->key, id);
+
+    // Set the polynomial node in the hash node
+    new_node->poly = polynome;
+
+    // Set the hash node in the hash table array
     pt->array[index] = new_node;
+}
+
+void displayPolynomial(const poly_node_t *poly)
+{
+    const poly_node_t *currentTerm = poly;
+    int isFirstTerm = 1;
+
+    while (currentTerm != NULL)
+    {
+        if (!isFirstTerm)
+        {
+            if (currentTerm->coff.up >= 0)
+            {
+                printf(" + ");
+            }
+            else
+            {
+                printf(" - ");
+            }
+        }
+        else if (currentTerm->coff.up < 0)
+        {
+            printf("-");
+        }
+
+        if (currentTerm->pow == 0)
+        {
+            if (currentTerm->coff.down != 1)
+            {
+                printf("%d/%d", abs(currentTerm->coff.up), abs(currentTerm->coff.down));
+            }
+            else
+            {
+                printf("%d", abs(currentTerm->coff.up));
+            }
+        }
+        else
+        {
+            if (abs(currentTerm->coff.up) != 1 || currentTerm->pow == 0)
+            {
+                printf("%d", abs(currentTerm->coff.up));
+            }
+
+            if (currentTerm->coff.down != 1 && currentTerm->pow != 0)
+            {
+                printf("/%d", currentTerm->coff.down);
+            }
+
+            if (currentTerm->pow != 0)
+            {
+                printf("X");
+            }
+
+            if (currentTerm->pow > 1)
+            {
+                printf("^%d", currentTerm->pow);
+            }
+        }
+
+        isFirstTerm = 0;
+        currentTerm = currentTerm->next;
+    }
+
+    printf("\n");
 }
 
 void poly_table_print(hash_table_t *pt, char *key)
 {
     size_t i = poly_key_index(key, pt->size);
-    poly_node_t **tmp = pt->array[i]->poly;
+    poly_node_t *tmp = pt->array[i]->poly;
 
-    printf("%s :", key);
-    while (!(*tmp))
-    {
-        if ((*tmp)->coff.up)
-        {
-            printf("%d", (*tmp)->coff.up);
-            if ((*tmp)->coff.down != 1)
-                printf("/%d", (*tmp)->coff.down);
-        }
-        if ((*tmp)->pow)
-        {
-            printf("x");
-            if ((*tmp)->pow > 1)
-                printf("^%d", (*tmp)->pow);
-        }
-    }
-    printf("\n");
+    printf("%s : ", key);
+    displayPolynomial(tmp);
 }
 
 void poly_set(hash_table_t *pt, char *key, char *new_name)
 {
     size_t i = poly_key_index(key, pt->size);
-    poly_node_t **tmp = pt->array[i]->poly;
-    poly_node_t *new;
+    poly_node_t *tmp = pt->array[i]->poly;
+    poly_node_t *new_poly = create_polynome(new_name);
 
-    *(pt->array[i]->poly) = create_polynome(new_name);
-    while (*tmp){
-        new = (*tmp)->next;
-        free(*tmp);
-        *tmp = new;
+    // Free the existing linked list
+    while (tmp != NULL)
+    {
+        poly_node_t *next = tmp->next;
+        free(tmp);
+        tmp = next;
     }
+
+    // Update the pointer to the new polynomial
+    pt->array[i]->poly = new_poly;
 }
 
 void poly_table_delete(hash_table_t *pt)
@@ -190,6 +294,7 @@ void poly_table_delete(hash_table_t *pt)
         hash_node_t *current = pt->array[i];
         hash_node_t *temp = current;
         current = current;
+        free(temp->key);
         free(temp->poly);
         free(temp);
     }
