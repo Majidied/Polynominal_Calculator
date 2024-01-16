94732a8524574e6e044de7299f6084dbad9b0d8a
mohammed majidi <majidimajidi2003@outlook.com>
2024-01-16 02:41:00 +0100

    update main fonction

--- Operations.c
+++ Operations.c
@@ -1,311 +1,252 @@
 #include "Poly_project.h"
 
-quot    fract_sum(quot n1, quot n2)
-{
-    quot sum;
+quot fract_sum(quot n1, quot n2) {
+  quot sum;
 
-    if (n1.down == 1 && n2.down == 1)
-    {
-        sum.up = n1.up + n2.up;
-        sum.down = 1;
-    }
-    else if (n1.up && n2.up)
-    {
-        sum.up = n1.up * n2.down + n1.down * n2.up;
-        sum.down = n1.down * n2.down;
-    }
-    else if (n1.up)
-        sum = n2;
-    else
-        sum = n1;
-    simplifyFraction(&(sum.up), &(sum.down));
-    return (sum);
+  if (n1.down == 1 && n2.down == 1) {
+    sum.up = n1.up + n2.up;
+    sum.down = 1;
+  } else if (n1.up && n2.up) {
+    sum.up = n1.up * n2.down + n1.down * n2.up;
+    sum.down = n1.down * n2.down;
+  } else if (n1.up)
+    sum = n2;
+  else
+    sum = n1;
+  simplifyFraction(&(sum.up), &(sum.down));
+  return (sum);
 }
 
-quot    fract_sub(quot n1, quot n2)
-{
-    quot sum;
+quot fract_sub(quot n1, quot n2) {
+  quot sum;
 
-    if (n1.down == 1 && n2.down == 1)
-    {
-        sum.up = n1.up - n2.up;
-        sum.down = 1;
-    }
-    else if (n1.up && n2.up)
-    {
-        sum.up = n1.up * n2.down - n1.down * n2.up;
-        sum.down = n1.down * n2.down;
-    }
-    else if (n1.up)
-        sum = n2;
-    else
-        sum = n1;
-    simplifyFraction(&(sum.up), &(sum.down));
-    return (sum);
+  if (n1.down == 1 && n2.down == 1) {
+    sum.up = n1.up - n2.up;
+    sum.down = 1;
+  } else if (n1.up && n2.up) {
+    sum.up = n1.up * n2.down - n1.down * n2.up;
+    sum.down = n1.down * n2.down;
+  } else if (n1.up)
+    sum = n2;
+  else
+    sum = n1;
+  simplifyFraction(&(sum.up), &(sum.down));
+  return (sum);
 }
 
-void    free_polynode(poly_node_t *sum)
-{
-    poly_node_t *next;
+void free_polynode(poly_node_t *sum) {
+  poly_node_t *next;
 
-    while (sum)
-    {
-        next = sum->next;
-        free(sum);
-        sum = next;
-    }
+  while (sum) {
+    next = sum->next;
+    free(sum);
+    sum = next;
+  }
 }
 
-poly_node_t *add_poly(poly_node_t *p1, poly_node_t *p2)
-{
-    poly_node_t *sum = malloc(sizeof(poly_node_t));
+poly_node_t *add_poly(poly_node_t *p1, poly_node_t *p2) {
+  poly_node_t *sum = malloc(sizeof(poly_node_t));
 
-    if (!sum || (!p1 && !p2))
-    {
-        perror("memory alloacation failed");
-        return (NULL);
+  if (!sum || (!p1 && !p2)) {
+    perror("memory alloacation failed");
+    return (NULL);
+  }
+  sum->next = NULL;
+  poly_node_t *tmp = sum;
+  while (p1 || p2) {
+    if (p1 && p2 && p1->pow == p2->pow) {
+      tmp->pow = p1->pow;
+      tmp->coff = fract_sum(p1->coff, p2->coff);
+      p1 = p1->next;
+      p2 = p2->next;
+    } else if ((p1 && !p2) || p1->pow > p2->pow) {
+      tmp->pow = p1->pow;
+      tmp->coff = p1->coff;
+      p1 = p1->next;
+    } else if (p2) {
+      tmp->pow = p2->pow;
+      tmp->coff = p2->coff;
+      p2 = p2->next;
     }
-    sum->next = NULL;
-    poly_node_t *tmp = sum;
-    while (p1 || p2)
-    {
-        if (p1 && p2 && p1->pow == p2->pow)
-        {
-            tmp->pow = p1->pow;
-            tmp->coff = fract_sum(p1->coff, p2->coff);
-            p1 = p1->next;
-            p2 = p2->next;
-        }
-        else if ((p1 && !p2) || p1->pow > p2->pow)
-        {
-            tmp->pow = p1->pow;
-            tmp->coff = p1->coff;
-            p1 = p1->next;
-        }
-        else if (p2)
-        {
-            tmp->pow = p2->pow;
-            tmp->coff = p2->coff;
-            p2 = p2->next;
-        }
-        tmp->next = malloc(sizeof(poly_node_t));
-        if (!tmp->next)
-        {
-            free_polynode(sum);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
+    tmp->next = malloc(sizeof(poly_node_t));
+    if (!tmp->next) {
+      free_polynode(sum);
+      perror("Memory allocation failed in add_poly()\n");
+      return (NULL);
     }
-    return (sum);
+    tmp = tmp->next;
+    tmp->next = NULL;
+  }
+  return (sum);
 }
 
-poly_node_t *sub_poly(poly_node_t *p1, poly_node_t *p2)
-{
-    poly_node_t *sub = malloc(sizeof(poly_node_t));
+poly_node_t *sub_poly(poly_node_t *p1, poly_node_t *p2) {
+  poly_node_t *sub = malloc(sizeof(poly_node_t));
 
-    if (!sub)
-    {
-        perror("memory alloacation failed");
-        return (NULL);
-    }
-    sub->next = NULL;
-    poly_node_t *tmp = sub;
-    while (p1 || p2)
-    {
-        if (p1 && p2 && p1->pow == p2->pow)
-        {
-            if (p1->coff.up != p2->coff.up || p1->coff.down != p2->coff.down)
-            {
-                tmp->pow = p1->pow;
-                tmp->coff = fract_sub(p1->coff, p2->coff);
-                p1 = p1->next;
-                p2 = p2->next;
-            }
-        }
-        else if ((p1 && !p2) || p1->pow > p2->pow)
-        {
-            tmp->pow = p1->pow;
-            tmp->coff = p1->coff;
-            p1 = p1->next;
-        }
-        else if (p2)
-        {
-            tmp->pow = p2->pow;
-            tmp->coff.up = -p2->coff.up;
-            tmp->coff.down = p2->coff.down;
-            p2 = p2->next;
-        }
-        tmp->next = malloc(sizeof(poly_node_t));
-        if (!tmp->next)
-        {
-            free_polynode(sub);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
-    }
-    return (sub);
-}
-
-poly_node_t *pow_poly(poly_node_t *p1, int pw)
-{
-    poly_node_t *pow = malloc(sizeof(poly_node_t));
-
-    if (!pow || !p1)
-    {
-        perror("memory alloacation failed");
-        return (NULL);
-    }
-    pow->next = NULL;
-    poly_node_t *tmp = pow;
-    while (p1)
-    {
-        tmp->pow = p1->pow * pw;
-        tmp->coff = p1->coff;
-        p1 = p1->next;
-        tmp->next = malloc(sizeof(poly_node_t));
-        if (!tmp->next)
-        {
-            free_polynode(pow);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
-    }
-    return (pow);
-}
-
-poly_node_t *aff_poly(poly_node_t *p1)
-{
-    poly_node_t *cpy = malloc(sizeof(poly_node_t));
-
-    if (!cpy || !p1)
-    {
-        perror("memory alloacation failed");
-        return (NULL);
-    }
-    cpy->next = NULL;
-    poly_node_t *tmp = cpy;
-    while (p1)
-    {
+  if (!sub) {
+    perror("memory alloacation failed");
+    return (NULL);
+  }
+  sub->next = NULL;
+  poly_node_t *tmp = sub;
+  while (p1 || p2) {
+    if (p1 && p2 && p1->pow == p2->pow) {
+      if (p1->coff.up != p2->coff.up || p1->coff.down != p2->coff.down) {
         tmp->pow = p1->pow;
-        tmp->coff = p1->coff;
+        tmp->coff = fract_sub(p1->coff, p2->coff);
         p1 = p1->next;
-        tmp->next = malloc(sizeof(poly_node_t));
-        if (!tmp->next)
-        {
-            free_polynode(cpy);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
+        p2 = p2->next;
+      }
+    } else if ((p1 && !p2) || p1->pow > p2->pow) {
+      tmp->pow = p1->pow;
+      tmp->coff = p1->coff;
+      p1 = p1->next;
+    } else if (p2) {
+      tmp->pow = p2->pow;
+      tmp->coff.up = -p2->coff.up;
+      tmp->coff.down = p2->coff.down;
+      p2 = p2->next;
     }
-    return (cpy);
+    tmp->next = malloc(sizeof(poly_node_t));
+    if (!tmp->next) {
+      free_polynode(sub);
+      perror("Memory allocation failed in add_poly()\n");
+      return (NULL);
+    }
+    tmp = tmp->next;
+    tmp->next = NULL;
+  }
+  return (sub);
 }
 
-poly_node_t *der_poly(poly_node_t *p1)
-{
-    poly_node_t *der = malloc(sizeof(poly_node_t));
-
-    if (!der || !p1)
-    {
-        perror("memory alloacation failed");
-        return (NULL);
-    }
-    der->next = NULL;
-    poly_node_t *tmp = der;
-    while (p1)
-    {
-        if (p1->pow)
-        {
-            tmp->pow = p1->pow - 1;
-            tmp->coff.up = p1->coff.up * p1->pow;
-            tmp->coff.down = p1->coff.down;
-            simplifyFraction(&tmp->coff.up, &tmp->coff.down);
-            p1 = p1->next;
-            tmp->next = malloc(sizeof(poly_node_t));
-        }
-        else
-        {
-            tmp->pow = 0;
-            tmp->coff.up = 0;
-            tmp->coff.up = 1;
-            p1 = p1->next;
-            tmp->next = malloc(sizeof(poly_node_t));
-        }
-        if (!tmp->next)
-        {
-            free_polynode(der);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
-    }
-    return (der);
+poly_node_t *pow_poly(hash_table_t *pt, char *key, int pw) {
+  size_t i = poly_key_index(key, pt->size);
+  if (pt->array[i] == NULL) {
+    printf("No polynomial with the given name exists.\n");
+    return NULL;
+  }
+  poly_node_t *tmp = pt->array[i]->poly;
+  for (int j = 1; j < pw; j++) {
+    tmp = multiplyPolynomials(tmp, pt->array[i]->poly);
+  }
+  return (tmp);
 }
 
-poly_node_t *inter_poly(poly_node_t *p1)
-{
-    poly_node_t *inter = malloc(sizeof(poly_node_t));
+poly_node_t *aff_poly(poly_node_t *p1) {
+  poly_node_t *cpy = malloc(sizeof(poly_node_t));
 
-    if (!inter || !p1)
-    {
-        perror("memory alloacation failed");
-        return (NULL);
+  if (!cpy || !p1) {
+    perror("memory alloacation failed");
+    return (NULL);
+  }
+  cpy->next = NULL;
+  poly_node_t *tmp = cpy;
+  while (p1) {
+    tmp->pow = p1->pow;
+    tmp->coff = p1->coff;
+    p1 = p1->next;
+    tmp->next = malloc(sizeof(poly_node_t));
+    if (!tmp->next) {
+      free_polynode(cpy);
+      perror("Memory allocation failed in add_poly()\n");
+      return (NULL);
     }
-    inter->next = NULL;
-    poly_node_t *tmp = inter;
-    while (p1)
-    {
-        if (p1->pow)
-        {
-            tmp->pow = p1->pow - 1;
-            tmp->coff.up = p1->coff.up;
-            tmp->coff.down = p1->coff.down * tmp->pow;
-            simplifyFraction(&tmp->coff.up, &tmp->coff.down);
-            p1 = p1->next;
-            tmp->next = malloc(sizeof(poly_node_t));
-        }
-        else
-        {
-            tmp->pow = 1;
-            tmp->coff = p1->coff;
-            p1 = p1->next;
-            tmp->next = malloc(sizeof(poly_node_t));
-        }
-        if (!tmp->next)
-        {
-            free_polynode(inter);
-            perror("Memory allocation failed in add_poly()\n");
-            return (NULL);
-        }
-        tmp = tmp->next;
-        tmp->next = NULL;
-    }
-    return (inter);
+    tmp = tmp->next;
+    tmp->next = NULL;
+  }
+  return (cpy);
 }
 
-int power(int x, int p)
-{
-    if (p == 0)
-        return (1);
-    return (x * power(x, p - 1));
+poly_node_t *der_poly(poly_node_t *p1) {
+  poly_node_t *der = malloc(sizeof(poly_node_t));
+
+  if (!der || !p1) {
+    perror("memory alloacation failed");
+    return (NULL);
+  }
+  der->next = NULL;
+  poly_node_t *tmp = der;
+  while (p1) {
+    if (p1->pow) {
+      tmp->pow = p1->pow - 1;
+      tmp->coff.up = p1->coff.up * p1->pow;
+      tmp->coff.down = p1->coff.down;
+      simplifyFraction(&tmp->coff.up, &tmp->coff.down);
+      p1 = p1->next;
+      tmp->next = malloc(sizeof(poly_node_t));
+    } else {
+      tmp->pow = 0;
+      tmp->coff.up = 0;
+      tmp->coff.up = 1;
+      p1 = p1->next;
+      tmp->next = malloc(sizeof(poly_node_t));
+    }
+    if (!tmp->next) {
+      free_polynode(der);
+      perror("Memory allocation failed in add_poly()\n");
+      return (NULL);
+    }
+    tmp = tmp->next;
+    tmp->next = NULL;
+  }
+  return (der);
 }
 
-int eval_poly(poly_node_t *p1, int x)
-{
+poly_node_t *inter_poly(poly_node_t *p1) {
+  poly_node_t *inter = malloc(sizeof(poly_node_t));
+
+  if (!inter || !p1) {
+    perror("memory alloacation failed");
+    return (NULL);
+  }
+  inter->next = NULL;
+  poly_node_t *tmp = inter;
+  while (p1) {
+    if (p1->pow) {
+      tmp->pow = p1->pow - 1;
+      tmp->coff.up = p1->coff.up;
+      tmp->coff.down = p1->coff.down * tmp->pow;
+      simplifyFraction(&tmp->coff.up, &tmp->coff.down);
+      p1 = p1->next;
+      tmp->next = malloc(sizeof(poly_node_t));
+    } else {
+      tmp->pow = 1;
+      tmp->coff = p1->coff;
+      p1 = p1->next;
+      tmp->next = malloc(sizeof(poly_node_t));
+    }
+    if (!tmp->next) {
+      free_polynode(inter);
+      perror("Memory allocation failed in add_poly()\n");
+      return (NULL);
+    }
+    tmp = tmp->next;
+    tmp->next = NULL;
+  }
+  return (inter);
+}
+
+int power(int x, int p) {
+  if (p == 0)
+    return (1);
+  return (x * power(x, p - 1));
+}
+
+int eval_poly(hash_table_t *pt, char *key, int x) {
+    size_t i = poly_key_index(key, pt->size);
+    if (pt->array[i] == NULL) {
+        printf("No polynomial with the given name '%s' exists.\n", key);
+        return 0;
+    }
+
+    poly_node_t *p1 = pt->array[i]->poly;
     int res = 0;
 
-    while(p1)
-    {
-        res += p1->coff.up / p1->coff.down * power(x, p1->pow);
+    while (p1) {
+        res += (p1->coff.up * power(x, p1->pow)) / p1->coff.down;
         p1 = p1->next;
     }
-    return (res);
+
+    return res;
 }
+
--- PrimaryOperators.c
+++ PrimaryOperators.c
@@ -1,306 +1,255 @@
 #include "Poly_project.h"
-hash_table_t *poly_table_create(size_t size)
-{
-    hash_table_t *pt = malloc(sizeof(hash_table_t));
-    if (pt == NULL)
-    {
-        perror("Failed to allocate memory for polynomial table");
-        exit(EXIT_FAILURE);
-    }
+hash_table_t *poly_table_create(size_t size) {
+  hash_table_t *pt = malloc(sizeof(hash_table_t));
+  if (pt == NULL) {
+    perror("Failed to allocate memory for polynomial table");
+    exit(EXIT_FAILURE);
+  }
 
-    pt->size = size;
-    pt->array = calloc(size, sizeof(hash_node_t *));
+  pt->size = size;
+  pt->array = calloc(size, sizeof(hash_node_t *));
 
-    if (pt->array == NULL)
-    {
-        perror("Failed to allocate memory for polynomial table array");
-        exit(EXIT_FAILURE);
-    }
+  if (pt->array == NULL) {
+    perror("Failed to allocate memory for polynomial table array");
+    exit(EXIT_FAILURE);
+  }
 
-    return pt;
+  return pt;
 }
 
-size_t poly_hash(char *name)
-{
-    int c;
-    size_t hash = 5381;
+size_t poly_hash(char *name) {
+  int c;
+  size_t hash = 5381;
 
-    while ((c = *name++))
-    {
-        hash = ((hash << 5) + hash) + c;
-    }
+  while ((c = *name++)) {
+    hash = ((hash << 5) + hash) + c;
+  }
 
-    return hash;
+  return hash;
 }
 
-size_t poly_key_index(char *name, size_t size)
-{
-    return poly_hash(name) % size;
+size_t poly_key_index(char *name, size_t size) {
+  return poly_hash(name) % size;
 }
 
-poly_node_t *addTerm(poly_node_t *poly, poly_node_t *term)
-{
-    poly_node_t *current = poly;
-    poly_node_t *prev = NULL;
+poly_node_t *addTerm(poly_node_t *poly, poly_node_t *term) {
+  poly_node_t *current = poly;
+  poly_node_t *prev = NULL;
 
-    while (current != NULL && current->pow > term->pow) {
-        prev = current;
-        current = current->next;
-    }
+  while (current != NULL && current->pow > term->pow) {
+    prev = current;
+    current = current->next;
+  }
 
-    if (current != NULL && current->pow == term->pow) {
-        current->coff.up = current->coff.up * term->coff.down + term->coff.up * current->coff.down;
-        current->coff.down *= term->coff.down;
-        simplifyFraction(&current->coff.up, &current->coff.down);
+  if (current != NULL && current->pow == term->pow) {
+    current->coff.up =
+        current->coff.up * term->coff.down + term->coff.up * current->coff.down;
+    current->coff.down *= term->coff.down;
+    simplifyFraction(&current->coff.up, &current->coff.down);
 
-        free(term);
-        return poly;
-    }
-
-    if (prev == NULL) {
-        term->next = poly;
-        return term;
-    }
-    prev->next = term;
-    term->next = current;
-
+    free(term);
     return poly;
+  }
+
+  if (prev == NULL) {
+    term->next = poly;
+    return term;
+  }
+  prev->next = term;
+  term->next = current;
+
+  return poly;
 }
 
-void parseNumber(const char *str, int *numerator, int *denominator)
-{
+void parseNumber(const char *str, int *numerator, int *denominator) {
+  char temp[100];
+  int i = 0;
+
+  while (*str != '+' && *str != '-' && *str != '\0') {
+    temp[i] = *str;
+    str++;
+    i++;
+  }
+
+  temp[i] = '\0';
+
+  if (strchr(temp, '/')) {
+    sscanf(temp, "%d/%d", numerator, denominator);
+  } else if (sscanf(temp, "%d", numerator) == 1) {
+    *denominator = 1;
+  } else {
+    *numerator = 1;
+    *denominator = 1;
+  }
+}
+
+poly_node_t *create_polynome(char *input) {
+  poly_node_t *poly = NULL;
+
+  while (*input == ' ') {
+    input++;
+  }
+
+  while (*input != '\0') {
+    int isNegative = 0;
+
+    if (*input == '-') {
+      isNegative = 1;
+      input++;
+    } else if (*input == '+') {
+      input++;
+    }
+
+    poly_node_t *term = (poly_node_t *)malloc(sizeof(poly_node_t));
+    term->pow = 0;
+    term->next = NULL;
+
     char temp[100];
     int i = 0;
 
-    while (*str != '+' && *str != '-' && *str != '\0')
-    {
-        temp[i] = *str;
-        str++;
-        i++;
+    while (*input != '+' && *input != '-' && *input != '\0') {
+      temp[i] = *input;
+      input++;
+      i++;
     }
 
     temp[i] = '\0';
 
-    if (strchr(temp, '/'))
-    {
-        sscanf(temp, "%d/%d", numerator, denominator);
+    char *varPart = strchr(temp, 'X');
+
+    if (varPart != NULL) {
+      if (sscanf(varPart, "X^%d", &term->pow) != 1) {
+        term->pow = 1;
+      }
     }
-    else if (sscanf(temp, "%d", numerator) == 1)
-    {
-        *denominator = 1;
-    }
-    else
-    {
-        *numerator = 1;
-        *denominator = 1;
-    }
+
+    int coefNumerator, coefDenominator;
+    parseNumber(temp, &coefNumerator, &coefDenominator);
+
+    term->coff.up = isNegative ? -coefNumerator : coefNumerator;
+    term->coff.down = coefDenominator;
+
+    poly = addTerm(poly, term);
+
+    size_t length = strcspn(input, "+-");
+    input += length;
+  }
+  return poly;
 }
 
-poly_node_t *create_polynome(char *input)
-{
-    poly_node_t *poly = NULL;
+void poly_table_set(hash_table_t *pt, char *id, char *name) {
+  poly_node_t *polynome = create_polynome(name);
 
-    while (*input == ' ')
-    {
-        input++;
-    }
+  size_t index = poly_key_index(id, pt->size);
+  if (pt->array[index] != NULL) {
+    printf("Error, already exist!\n");
+    return;
+  }
 
-    while (*input != '\0')
-    {
-        int isNegative = 0;
+  hash_node_t *new_node = malloc(sizeof(hash_node_t));
+  if (new_node == NULL) {
+    perror("Failed to allocate memory for polynomial node");
+    exit(EXIT_FAILURE);
+  }
 
-        if (*input == '-')
-        {
-            isNegative = 1;
-            input++;
-        }
-        else if (*input == '+')
-        {
-            input++;
-        }
+  new_node->key = malloc(strlen(id) + 1);
+  if (new_node->key == NULL) {
+    perror("Failed to allocate memory for key");
+    exit(EXIT_FAILURE);
+  }
+  strcpy(new_node->key, id);
 
-        poly_node_t *term = (poly_node_t *)malloc(sizeof(poly_node_t));
-        term->pow = 0;
-        term->next = NULL;
+  new_node->poly = polynome;
 
-        char temp[100];
-        int i = 0;
-
-        while (*input != '+' && *input != '-' && *input != '\0')
-        {
-            temp[i] = *input;
-            input++;
-            i++;
-        }
-
-        temp[i] = '\0';
-
-        char *varPart = strchr(temp, 'X');
-
-        if (varPart != NULL)
-        {
-            if (sscanf(varPart, "X^%d", &term->pow) != 1)
-            {
-                term->pow = 1;
-            }
-        }
-
-        int coefNumerator, coefDenominator;
-        parseNumber(temp, &coefNumerator, &coefDenominator);
-
-        term->coff.up = isNegative ? -coefNumerator : coefNumerator;
-        term->coff.down = coefDenominator;
-
-        poly = addTerm(poly, term);
-
-        size_t length = strcspn(input, "+-");
-        input += length;
-    }
-    return poly;
+  pt->array[index] = new_node;
 }
 
-void poly_table_set(hash_table_t *pt, char *id, char *name)
-{
-    // Create a new polynomial node and copy the polynomial data
-    poly_node_t *polynome = create_polynome(name);
+void displayPolynomial(const poly_node_t *poly) {
+  const poly_node_t *currentTerm = poly;
+  int isFirstTerm = 1;
 
-    // Find the index in the hash table array
-    size_t index = poly_key_index(id, pt->size);
-
-    // Create a new hash node and allocate memory for the key
-    hash_node_t *new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        perror("Failed to allocate memory for polynomial node");
-        exit(EXIT_FAILURE);
+  while (currentTerm != NULL) {
+    if (!isFirstTerm) {
+      if (currentTerm->coff.up >= 0) {
+        printf(" + ");
+      } else {
+        printf(" - ");
+      }
+    } else if (currentTerm->coff.up < 0) {
+      printf("-");
     }
 
-    // Allocate memory for the key and copy it
-    new_node->key = malloc(strlen(id) + 1);
-    if (new_node->key == NULL)
-    {
-        perror("Failed to allocate memory for key");
-        exit(EXIT_FAILURE);
-    }
-    strcpy(new_node->key, id);
+    if (currentTerm->pow == 0) {
+      if (currentTerm->coff.down != 1) {
+        printf("%d/%d", abs(currentTerm->coff.up), abs(currentTerm->coff.down));
+      } else {
+        printf("%d", abs(currentTerm->coff.up));
+      }
+    } else {
+      if (abs(currentTerm->coff.up) != 1 || currentTerm->pow == 0) {
+        printf("%d", abs(currentTerm->coff.up));
+      }
 
-    // Set the polynomial node in the hash node
-    new_node->poly = polynome;
-
-    // Set the hash node in the hash table array
-    pt->array[index] = new_node;
-}
-
-void displayPolynomial(const poly_node_t *poly)
-{
-    const poly_node_t *currentTerm = poly;
-    int isFirstTerm = 1;
-
-    while (currentTerm != NULL)
-    {
-        if (!isFirstTerm)
-        {
-            if (currentTerm->coff.up >= 0)
-            {
-                printf(" + ");
-            }
-            else
-            {
-                printf(" - ");
-            }
+      if (currentTerm->coff.down != 1 && currentTerm->pow != 0) {
+        if (currentTerm->coff.up == 1) {
+          printf("%d", currentTerm->coff.up);
         }
-        else if (currentTerm->coff.up < 0)
-        {
-            printf("-");
-        }
+        printf("/%d", currentTerm->coff.down);
+      }
 
-        if (currentTerm->pow == 0)
-        {
-            if (currentTerm->coff.down != 1)
-            {
-                printf("%d/%d", abs(currentTerm->coff.up), abs(currentTerm->coff.down));
-            }
-            else
-            {
-                printf("%d", abs(currentTerm->coff.up));
-            }
-        }
-        else
-        {
-            if (abs(currentTerm->coff.up) != 1 || currentTerm->pow == 0)
-            {
-                printf("%d", abs(currentTerm->coff.up));
-            }
+      if (currentTerm->pow != 0) {
+        printf("X");
+      }
 
-            
-
-            if (currentTerm->coff.down != 1 && currentTerm->pow != 0)
-            {
-                if (currentTerm->coff.up == 1)
-            {
-                printf("%d", currentTerm->coff.up);
-            }
-                printf("/%d", currentTerm->coff.down);
-            }
-
-            if (currentTerm->pow != 0)
-            {
-                printf("X");
-            }
-
-            if (currentTerm->pow > 1)
-            {
-                printf("^%d", currentTerm->pow);
-            }
-        }
-
-        isFirstTerm = 0;
-        currentTerm = currentTerm->next;
+      if (currentTerm->pow > 1) {
+        printf("^%d", currentTerm->pow);
+      }
     }
 
-    printf("\n");
+    isFirstTerm = 0;
+    currentTerm = currentTerm->next;
+  }
+
+  printf("\n");
 }
 
-void poly_table_print(hash_table_t *pt, char *key)
-{
-    size_t i = poly_key_index(key, pt->size);
-    poly_node_t *tmp = pt->array[i]->poly;
+void poly_table_print(hash_table_t *pt, char *key) {
+  size_t i = poly_key_index(key, pt->size);
+  poly_node_t *tmp = pt->array[i]->poly;
 
-    printf("%s : ", key);
-    displayPolynomial(tmp);
+  printf("%s : ", key);
+  displayPolynomial(tmp);
 }
 
-void poly_set(hash_table_t *pt, char *key, char *new_name)
-{
-    size_t i = poly_key_index(key, pt->size);
-    poly_node_t *tmp = pt->array[i]->poly;
-    poly_node_t *new_poly = create_polynome(new_name);
+void poly_set(hash_table_t *pt, char *key, char *new_name) {
+  size_t i = poly_key_index(key, pt->size);
+  if (pt->array[i] == NULL) {
+    printf("No polynomial with the given name exists.\n");
+    return;
+  }
 
-    // Free the existing linked list
-    while (tmp != NULL)
-    {
-        poly_node_t *next = tmp->next;
-        free(tmp);
-        tmp = next;
-    }
+  poly_node_t *tmp = pt->array[i]->poly;
+  poly_node_t *new_poly = create_polynome(new_name);
 
-    // Update the pointer to the new polynomial
-    pt->array[i]->poly = new_poly;
+  while (tmp != NULL) {
+    poly_node_t *next = tmp->next;
+    free(tmp);
+    tmp = next;
+  }
+
+  pt->array[i]->poly = new_poly;
 }
 
-void poly_table_delete(hash_table_t *pt)
-{
-    for (size_t i = 0; i < pt->size; ++i)
-    {
-        hash_node_t *current = pt->array[i];
-        hash_node_t *temp = current;
-        current = current;
-        free(temp->key);
-        free(temp->poly);
-        free(temp);
-    }
+void poly_table_delete(hash_table_t *pt) {
+  for (size_t i = 0; i < pt->size; ++i) {
+    hash_node_t *current = pt->array[i];
+    hash_node_t *temp = current;
+    current = current;
+    free(temp->key);
+    free(temp->poly);
+    free(temp);
+  }
 
-    free(pt->array);
-    free(pt);
+  free(pt->array);
+  free(pt);
 }
-
